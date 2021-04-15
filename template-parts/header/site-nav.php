55d14fbbd1b6c6af595bd90358d2c7ff02e85aad
tlahmann <tobiaslahmann@gmail.com>
2021-04-15 12:43:50 +0200

    feat: add collapsing hamburger menu

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -9,28 +9,24 @@
 
 ?>
 
-<?php if ( has_nav_menu( 'primary' ) ) : ?>
-	<nav id="site-navigation" class="primary-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'imm' ); ?>">
-		<!-- <div class="menu-button-container">
-			<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
-				<span class="dropdown-icon open"><?php //esc_html_e( 'Menu', 'imm' ); ?>
-					<i class="fas fa-bars"></i>
-				</span>
-				<span class="dropdown-icon close"><?php //esc_html_e( 'Close', 'imm' ); ?>
-					<i class="fas fa-times"></i>
-				</span>
-			</button><!-- #primary-mobile-menu ->
-		</div>.menu-button-container -->
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => 'primary',
-				'menu_class'      => 'menu-wrapper',
-				'container_class' => 'primary-menu-container',
-				'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-				'fallback_cb'     => false,
-			)
-		);
-		?>
+<?php if (has_nav_menu('primary')) : ?>
+    <nav id="site-navigation" class="primary-navigation" role="navigation" aria-label="<?php esc_attr_e('Primary menu', 'imm'); ?>">
+        <input type="checkbox" id="hamburg">
+        <label for="hamburg" class="hamburg">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+        </label>
+        <?php
+        wp_nav_menu(
+            array(
+                        'theme_location'  => 'primary',
+                        'menu_class'      => 'menu-wrapper',
+                        'container_class' => 'primary-menu-container',
+                        'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+                        'fallback_cb'     => false,
+                    )
+        );
+        ?>
 	</nav><!-- #site-navigation -->
 <?php endif; ?>
