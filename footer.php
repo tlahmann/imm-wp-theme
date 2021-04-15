4121c6df1a1d240378ce08d5f12255f00c3f67c7
tlahmann <tobiaslahmann@gmail.com>
2021-04-15 14:34:10 +0200

    feat: style footer

--- footer.php
+++ footer.php
@@ -2,7 +2,31 @@
 </main> <!-- close container -->
 
 <footer>
-<h1>FOOTER</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="two columns"><a href="/"><?php the_custom_logo(); ?></a></div>
+        <ul class="row eight columns offset-by-two">
+            <li class="three columns">
+                <a ui-sref="home">INSTITUT</a>
+            </li>
+            <li class="three columns">
+                <a ui-sref="teaching">Lehre</a>
+            </li>
+            <li class="three columns">
+                <a ui-sref="archive">Archiv</a>
+            </li>
+            <li class="three columns">
+                <a ui-sref="contact">Kontakt</a>
+                <ul>
+                    <li><a id="contact" href="/contact">Kontakt</a></li>
+                    <li><a id="imprint" href="/imprint">Impressum</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+
 </footer>
 
 </body>
