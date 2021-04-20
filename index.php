695764d74e77068287cc34b39176030a120c9727
tlahmann <tobiaslahmann@gmail.com>
2021-04-20 20:01:06 +0200

    feat: display intro text on index

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
 
 <hr/>
 
+<div class="row">
+    <strong>
+        INTRO TEXT:
+    </strong>
+    <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt 
+        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et 
+        ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum 
+        dolor sit amet.
+    </p>
+</div>
+
 <?php get_template_part('template-parts/projects'); ?>
 
 <?php get_footer(); ?>
