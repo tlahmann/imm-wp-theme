f20026e60489d2743e6c3452cc34ea7b30887740
tlahmann <tobiaslahmann@gmail.com>
2021-04-20 18:43:23 +0200

    feat: display cpt terms in project sidebar

--- sidebar-project.php
+++ sidebar-project.php
@@ -1,48 +1,30 @@
-<h5>Project Sidebar</h5>
-
 <?php
-
 $queried_object = get_queried_object();
+if ($queried_object) :
+    $terms = wp_get_post_terms($queried_object->ID, array( 'project_categories' ));
+    ?>
 
-if ($queried_object) {
+    <!-- Print all tech facts -->
+    <strong>Tech. Facts:</strong>
+    <ul>
+    <?php foreach ($terms as $term) : ?>
+        <li><?php echo $term->name; ?></li>
+    <?php endforeach; ?>
+    </ul>
+
+    <!-- Print all attachment images -->
+    <?php
     $post_id = $queried_object->ID;
-    echo get_the_title($post_id);
 
     $media_set = get_post_meta($post_id, '_media_ids', true);
     $media_set = explode(',', $media_set);
 
-    $attachments = get_posts( array(
+    $attachments = get_posts(array(
         'post_type'      => 'attachment',
         'posts_per_page' => -1,
         'include'        => $media_set
-    ) );
-
-    // var_dump($attachments);
-
+    ));
     foreach ($attachments as $key => $value) {
         echo wp_get_attachment_image($value->ID);
-        // echo $value->id;
     }
-
-    // $post_meta = get_post_meta($post_id);
-    // var_dump($post_meta);
-    // // echo $post_meta['_subject_id'][0];
-    // $subject_id = $post_meta['_subject_id'][0];
-
-    // echo 'Projekt durchgeführt in Semester: ' . $post_meta['_term'][0] . '<br/>';
-
-    // $results = new WP_Query(array(
-    //     'post_type' => 'subject',
-    //     'p' => $subject_id)
-    // );// need to set the simple quote '$id'
-      
-    // if ($results->have_posts()) : while ($results->have_posts()) : $results->the_post();
-    // echo 'Projekt durchgeführt in Fach: '; the_title();
-    // endwhile; else:
-    // _e('Sorry, no impression or project found.');
-    // endif;
-    
-}
-
-
-?>
+endif; ?>
--- single-project.php
+++ single-project.php
@@ -30,28 +30,13 @@ while (have_posts()) :
     <div class="row">
         <article id="post-<?php the_ID(); ?>" <?php post_class(['eight columns']); ?> >
 
-
         <div class="entry-content">
             <?php
             the_content();
-
-            wp_link_pages(
-                array(
-                    'before'   => '<nav class="page-links" aria-label="' . esc_attr__('Page', 'twentytwentyone') . '">',
-                    'after'    => '</nav>',
-                    /* translators: %: Page number. */
-                    'pagelink' => esc_html__('Page %', 'twentytwentyone'),
-                )
-            );
             ?>
         </div><!-- .entry-content -->
 
-
-        <?php if (! is_singular('attachment')) : ?>
-            <?php //get_template_part( 'template-parts/post/subject-bio' );?>
-        <?php endif; ?>
-
-        </article><!-- #post-<?php the_ID(); ?> -->
+        </article>
 
         <aside class="four columns">
             <?php //var_dump(get_post_meta()); ?>
